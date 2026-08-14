98653d1bd32a516e2ba488172db4b0ff7f9a23c1
Andrzej Ból <andrzej.bol@gmail.com>
2026-08-14 20:09:27 +0200

    Wersja 2.0.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         applicationId = "com.nexuscontroller.pad"
         minSdk = 28
         targetSdk = 34
-        versionCode = 20003
-        versionName = "2.0.3"
+        versionCode = 20004
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
